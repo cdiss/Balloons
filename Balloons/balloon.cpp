13a903f49250e395c11dcc92d14bab3e2308913c
Lanya Butler <llbutler@colby.edu>
2013-06-15 20:30:15 -0500

    Added all balloon colors

--- Balloons/balloon.cpp
+++ Balloons/balloon.cpp
@@ -7,10 +7,14 @@ using namespace std;
 
 Balloon::Balloon(BalloonColor aColor) {
   if (aColor == NONE) {
-    int newColor_int = rand()%2;
+    int newColor_int = rand()%4;
     if (newColor_int == 0) {
       aColor = RED;
-    } else {
+    } else if (newColor_int == 1){
+        aColor = BLUE;
+    } else if (newColor_int == 2){
+        aColor = GREEN;
+    }else {
       aColor = YELLOW;
     }
   }
@@ -23,6 +27,8 @@ Balloon::Balloon(BalloonColor aColor) {
 
 GLMmodel* Balloon::pmodel = NULL;
 GLMmodel* Balloon::pmodelYellow = NULL;
+GLMmodel* Balloon::pmodelBlue = NULL;
+GLMmodel* Balloon::pmodelGreen = NULL;
 
 void Balloon::randomizeForStart() {
   pos[1] = rand()%150 - 100.0f;
@@ -40,7 +46,9 @@ void Balloon::draw() {
   case YELLOW:
     glmDraw(pmodelYellow, GLM_SMOOTH | GLM_MATERIAL);
   case GREEN:
+    glmDraw(pmodelGreen, GLM_SMOOTH | GLM_MATERIAL);
   case BLUE:
+    glmDraw(pmodelBlue, GLM_SMOOTH | GLM_MATERIAL);
   default:
     break;
   }
@@ -62,3 +70,13 @@ void Balloon::setModel(GLMmodel* aModel) {
 void Balloon::setModelYellow(GLMmodel* aModel){
     pmodelYellow = aModel;
 }
+
+void Balloon::setModelBlue(GLMmodel* aModel){
+    pmodelBlue = aModel;
+}
+
+void Balloon::setModelGreen(GLMmodel* aModel){
+    pmodelGreen = aModel;
+}
+
+
